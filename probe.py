e2eb5b8d58e2f266aa6bc2a78534ab7ff71a1482
SergeySadykovN <snsadykov@ya.ru>
2023-11-29 14:22:56 +0300

    test mac os

--- probe.py
+++ probe.py
@@ -1,4 +1,14 @@
-a = 'Big'
-b = 'Small'
 
-print (a < b)
+# вписать имя и фамилию , определить  мужчина или женщина
+name_1 = input('input name and fullname ')
+
+print (name_1)
+
+if name_1.isnumeric():
+    print ('is not name')
+elif name_1.isalpha():
+    print('Thanks')
+else:
+    print('error')
+
+

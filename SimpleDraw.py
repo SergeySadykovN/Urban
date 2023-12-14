31731e2924cb0645cf90f5f7a197362162b298fd
Sergey Sadykov <snsadykov@ya.ru>
2023-12-14 19:16:43 +0300

    vers prepod

--- SimpleDraw.py
+++ SimpleDraw.py
@@ -1,9 +1,20 @@
 import simple_draw as sd
+from math import cos, sin, pi
+from time import sleep
 
-sd.resolution = (1200, 600)
+angle = 0
+length = 100
+x = y = 300
+print(sin(90/180*pi))
+for i in range(40):
+    point = sd.get_point(x, y)
+    v = sd.get_vector(start_point=point, length=length, angle=angle)
+    v.draw()
+    sd.circle(center_position=point, radius=10*cos(angle))
+    print(point, x, y)
+    x += length*cos(angle/180*pi)
+    y += length*sin(angle/180*pi)
+    angle += 110
+    sleep(0.1)
 
-point = sd.get_point(100, 100)
-radius = 50
-for _ in range(3):
-    radius += 5
-    sd.circle(center_position=point, radius=radius)
+sd.pause()

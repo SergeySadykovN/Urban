43c33729028122c2d925c18a3f37bb698b7becee
SergeySadykovN <snsadykov@ya.ru>
2023-12-23 15:34:52 +0300

    23.12.2023\vers 1

---
+++ AudioFromYoutube.py
@@ -0,0 +1,36 @@
+from pytube import YouTube
+from tkinter import *
+from tkinter import ttk
+
+
+def download_link(link):
+    """download func"""
+    yt = YouTube(link, use_oauth=True)
+    stream = yt.streams.get_by_itag(251)
+    return stream.download(output_path='D:\Downloads')
+
+
+def window_api():
+    """window_api func"""
+
+    def get_link():
+        link = entry.get()
+        entry.delete(0,END)
+        return download_link(link)
+
+    root = Tk()
+    root.title("Youtube Downloader")
+    root.geometry("400x150")
+
+    label = Label(text="Input link: ")
+    label.pack()
+
+    entry = ttk.Entry()
+    entry.pack(padx=20, fill=X)
+
+    btn = ttk.Button(text="Download", command=get_link)
+    btn.pack(pady=30)
+    root.mainloop()
+
+
+window_api()

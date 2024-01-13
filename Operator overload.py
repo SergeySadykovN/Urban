9a64d1c456e1b5422c43139a93ba1096589c1916
SergeySadykovN <snsadykov@ya.ru>
2024-01-13 15:39:57 +0300

    13.01.2024

---
+++ Operator overload.py
@@ -0,0 +1,25 @@
+class Building:
+    def __init__(self, numberOfFloors, buildingType):
+        self.numberOfFloors = int(numberOfFloors)
+        self.buildingType = str(buildingType)
+
+    def __eq__(self, other):
+        return self.numberOfFloors == other
+
+
+firstBuilding = Building(10, 'simple building')
+secondBuilding = Building(100, 'skyscraper')
+thirdBuilding = Building(10, 'simple building')
+fourthBuilding = Building(10, 'not simple building')
+
+#  обращение к методу класса  через __eq__
+if Building.__eq__(firstBuilding, thirdBuilding):
+    print('identical')
+else:
+    print('not identical!')
+
+# прямое сравнение экземпляров
+if firstBuilding == secondBuilding:
+    print('equal')
+else:
+    print('not equal!')

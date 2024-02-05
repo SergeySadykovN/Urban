aa3873a2908f1e52e31f130a4ef0d464ca24e7ae
SergeySadykovN <snsadykov@ya.ru>
2024-02-05 17:27:31 +0300

    05/02/2024

--- file_opening_mode.py
+++ file_opening_mode.py
@@ -1,51 +1,51 @@
-# Открываем файл с латинскими и русскими символами и выводим в консоль (mode='r')
+# # Открываем файл с латинскими и русскими символами и выводим в консоль (mode='r')
 import io
 import operator
 import os
 from itertools import count
 
-poetry = 'poetry.txt'
-file = open(poetry, mode='r', encoding='utf8')
-content = file.read()
-file.close()
-# print(content)
-
-# Создаем пустой файл и записываем латинские и русские символы (mode='w')
-# file_name = 'out.txt'
-# file = open(file_name, mode='w', encoding='utf8')
-# content1 = 'Hello World!, Привет, мир! '
-# file.write(content1)
-# file.close()
-#
-# # Открываем файл, читаем и добавляем в конец символы (mode='a')
-# file = open(poetry, mode='a', encoding='utf8')
-# file_content = '\nPeace for everyone! '
-# file.write(file_content)
-# file.close()
-#
-# # Режим чтения и записи одновременно (mode='r+')
-# file = open(poetry, mode='r+', encoding='utf8')
-# content2 = file.read()
-# file.write('\nNo  war!')
-# file.close()
-
-# Позиционный вывод
+# poetry = 'poetry.txt'
 # file = open(poetry, mode='r', encoding='utf8')
+# content = file.read()
+# file.close()
+# # print(content)
 #
-# # new_position = file.seek(0, os.SEEK_CUR)
-# file_content1 = file.read(37)#до какого номера символа считываем
-# print(file_content1)
-# print(file.tell()) #показывает на каком пот счету символе остановились
+# # Создаем пустой файл и записываем латинские и русские символы (mode='w')
+# # file_name = 'out.txt'
+# # file = open(file_name, mode='w', encoding='utf8')
+# # content1 = 'Hello World!, Привет, мир! '
+# # file.write(content1)
+# # file.close()
+# #
+# # # Открываем файл, читаем и добавляем в конец символы (mode='a')
+# # file = open(poetry, mode='a', encoding='utf8')
+# # file_content = '\nPeace for everyone! '
+# # file.write(file_content)
+# # file.close()
+# #
+# # # Режим чтения и записи одновременно (mode='r+')
+# # file = open(poetry, mode='r+', encoding='utf8')
+# # content2 = file.read()
+# # file.write('\nNo  war!')
+# # file.close()
 #
-# file_content1 = file.read()
-# print(file_content1)
-# print(file.tell())
-
-
-# with open(poetry, mode='r', encoding='utf8') as file:
-#     file_content3 = file.read()
-#     print(file_content3)
-
+# # Позиционный вывод
+# # file = open(poetry, mode='r', encoding='utf8')
+# #
+# # # new_position = file.seek(0, os.SEEK_CUR)
+# # file_content1 = file.read(37)#до какого номера символа считываем
+# # print(file_content1)
+# # print(file.tell()) #показывает на каком пот счету символе остановились
+# #
+# # file_content1 = file.read()
+# # print(file_content1)
+# # print(file.tell())
+#
+#
+# # with open(poetry, mode='r', encoding='utf8') as file:
+# #     file_content3 = file.read()
+# #     print(file_content3)
+#
 file_name = 'Poem about Winter.txt'
 with open(file_name, mode='w', encoding='utf8') as file:
     content_add = ('Разукрасилась зима: На уборе бахрома \n'
@@ -73,18 +73,41 @@ with open(file_name, mode='r', encoding='utf8') as file:
         else:
             symbol_count[char] = 1
 
+    
+
     print(symbol_count)
 
-    new_position = file.seek(0, os.SEEK_SET)
-    file_content = file.read(20)
 
-with open(file_name, mode='r', encoding='utf8') as file:
-    line = True
-    while line:
-        line = file.readline()
-        if 'Дед' in line:
-            print("word in line:", line)
-            break
-    else:
-        print("not in file")
+
+
+
+
+
+#
+#     new_position = file.seek(0, os.SEEK_SET)
+#     file_content = file.read(20)
+#
+# with open(file_name, mode='r', encoding='utf8') as file:
+#     line = True
+#     while line:
+#         line = file.readline()
+#         if 'Дед' in line:
+#             print("word in line:", line)
+#             break
+#     else:
+#         print("not in file")
+# #
+# from pypdf import PdfReader
+#
+# file_path = r'C:\Users\Programmer\Desktop\MonteCristo.pdf'
+# with open(file_path, 'r', encoding='utf8') as file:
+#
+#     reader = PdfReader(file_path)
+#     number_of_page = len(reader.pages)
+#     page = reader.pages[9]
+#     text_from_pdf = page.extract_text()
+#     print(number_of_page, text_from_pdf)
+
+
+
 

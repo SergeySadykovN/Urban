cd2ce5e07da300d350c6023709be21d1e3744c36
Sergey Sadykov <snsadykov@ya.ru>
2024-02-07 16:13:09 +0300

    07\02\24

--- Warning_Except.py
+++ Warning_Except.py
@@ -1,5 +1,12 @@
 import warnings
 
+'''Импортируйте модуль warnings.
+Реализуйте функцию для расчёта деления, которая будет генерировать предупреждение, 
+если делитель близок к нулю (например, меньше 0.01), предупреждая об опасности деления на ноль.
+Сгенерируйте UserWarning в этом случае.
+Используйте разные фильтры для управления поведением программы 
+при появлении такого предупреждения: always, ignore, error'''
+
 
 def division_function(dividend, divisor):
     if divisor <= 0.01:
@@ -7,8 +14,8 @@ def division_function(dividend, divisor):
     return dividend / divisor
 
 
-warnings.filterwarnings('always') #выбрасывает варнинг всегда
-# warnings.filterwarnings('ignore') #игнорирует варнинг
+warnings.filterwarnings('always')  # выбрасывает варнинг всегда
+# warnings.filterwarnings('ignore') # игнорирует варнинг
 
 print(division_function(10, 0.01))
 

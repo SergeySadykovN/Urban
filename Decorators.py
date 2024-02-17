effddcfe576d2a127ec76e196afc23de135a694e
SergeySadykovN <snsadykov@ya.ru>
2024-02-17 16:17:03 +0300

    17/02/2024

--- Decorators.py
+++ Decorators.py
@@ -12,11 +12,11 @@ def is_prime(func):
         elif n == 1:
             return f'{n} is prime'
         elif n == 2:
-            return f'{n} is not prime'
+            return f'{n} is composite'
         else:
             for i in range(2, n):
                 if n % i == 0:
-                    return f'{n} is not prime'
+                    return f'{n} is composite'
                 return f'{n} is prime'
 
     return wrapper
@@ -29,5 +29,5 @@ def sum_three(a, b, c):
 
 # print(sum_three(0, 0, -1))# Exception: negative numbers cannot be simple or composite
 # print(sum_three(0, 0, 0))# The number 0 is neither a prime nor a composite number
-print(sum_three(1, 2, 3))  # 6 is not prime
+print(sum_three(1, 2, 3))  # 6 is composite
 print(sum_three(0, 0, 1))  # 1 is prime

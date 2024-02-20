76d94e513645a5bc074cd3b8fc2ac7bafcf1807e
SergeySadykovN <snsadykov@ya.ru>
2024-02-20 10:47:17 +0300

    20/02/2024 __iter__ алгоритм вычисления четных чисел __next__ выполняем шаг +=2

---
+++ Iterators_2.py
@@ -0,0 +1,26 @@
+class Evennumbers:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.value = start
+
+    def __iter__(self):
+        if self.start % 2 == 1:
+            self.value = self.start - 1
+        else:
+            self.value = self.start
+        return self
+
+    def __next__(self):
+        if self.value > self.end:
+            raise StopIteration
+        else:
+            self.value += 2
+            return self.value
+
+
+evn = Evennumbers(1, 7)
+for i in evn:
+    print(i)
+for i in evn:
+    print(i)

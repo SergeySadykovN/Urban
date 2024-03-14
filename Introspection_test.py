2457039bd2d42bcf9e3ab38c2ee6d27caa1f5794
SergeySadykovN <snsadykov@ya.ru>
2024-03-14 18:30:19 +0300

    14.03.2024

--- Introspection_test.py
+++ Introspection_test.py
@@ -1,9 +1,17 @@
 import inspect
 from pprint import pprint
-import faker
 
 
-class Man_can:
+class Man:
+    hair = True
+    gender = None
+
+    def return_anything(self):
+        return 'This is a man'
+
+
+class Person(Man):
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
@@ -12,30 +20,22 @@ class Man_can:
         print(f'{self.name} age: {self.age}')
 
     def mean(self, age_list):
-        print(sum(age_list)/len(age_list))
-
-
+        print(sum(age_list) / len(age_list))
 
 
 def introspection_info(obj):
+    '''Функция с методами для инспектирования'''
     info = {}
     info['type'] = type(obj)
     info['module'] = obj.__module__
     info['attributes'] = [attr_name for attr_name, attr_value in vars(obj).items() if not callable(attr_value)]
     info['methods'] = [meth_name for meth_name in dir(obj) if not meth_name.startswith('__')]
-    if inspect.isclass(obj): # Проверяем, является ли объект классом
+    if inspect.isclass(obj):  # Проверяем, является ли объект классом
         info['class_attrs'] = ['__module__', '__name__', 'mro']
-    elif inspect.ismethod(obj): # Проверяем, является ли объект методом
+    elif inspect.ismethod(obj):  # Проверяем, является ли объект методом
         info['method_args'] = inspect.getfullargspec(obj)[0]
     return info
 
 
-
-
-
-
-
-obj = introspection_info(Man_can)
+obj = introspection_info(Person)
 pprint(obj)
-
-
